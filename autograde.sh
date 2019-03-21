96afe2e9e2848d505cc01a460954da7f3138ae46
Thomas Wade <wade0059@flinders.edu.au>
2019-03-21 17:44:22 +1030

    Add autograde script

---
+++ autograde.sh
@@ -0,0 +1,52 @@
+### Exercise 1
+# Clean it
+sudo rm -rf unit1exercise1/ unit1-solution1.tgz
+# Run it
+sudo bash unit1-exercise-1-solution.sh
+# Grade it
+sudo bash unit1-exercise-1-grade.sh unit1-exercise-1-solution.sh
+# Clean it again
+sudo rm -rf unit1exercise1/ unit1-solution1.tgz
+
+### Exercise 2
+# Clean it
+sudo rm -rf unit1exercise2/ unit1-solution2.tgz
+# Run it
+sudo bash unit1-exercise-2-solution.sh
+# Grade it
+sudo bash unit1-exercise-2-grade.sh unit1-exercise-2-solution.sh
+# Clean it again
+sudo rm -rf unit1exercise2/ unit1-solution2.tgz
+
+exit
+
+### Exercise 1
+# Clean it
+sudo rm -rf unit1exercise1/ unit1-solution1.tgz
+# Run it
+sudo bash unit1-exercise-1-solution.sh
+# Grade it
+sudo bash unit1-exercise-1-grade.sh unit1-exercise-1-solution.sh
+# Clean it again
+sudo rm -rf unit1exercise1/ unit1-solution1.tgz
+
+### Exercise 1
+# Clean it
+sudo rm -rf unit1exercise1/ unit1-solution1.tgz
+# Run it
+sudo bash unit1-exercise-1-solution.sh
+# Grade it
+sudo bash unit1-exercise-1-grade.sh unit1-exercise-1-solution.sh
+# Clean it again
+sudo rm -rf unit1exercise1/ unit1-solution1.tgz
+
+### Exercise 1
+# Clean it
+sudo rm -rf unit1exercise1/ unit1-solution1.tgz
+# Run it
+sudo bash unit1-exercise-1-solution.sh
+# Grade it
+sudo bash unit1-exercise-1-grade.sh unit1-exercise-1-solution.sh
+# Clean it again
+sudo rm -rf unit1exercise1/ unit1-solution1.tgz
+
