fb5a6e958b64e7d718b409e1c00b8c8aa512576a
Paul Gardner-Stephen <paul@servalproject.org>
2019-03-21 17:43:09 +1030

    unit1 files

--- unit1-exercise-1-grade.sh
+++ unit1-exercise-1-grade.sh
@@ -15,10 +15,10 @@ outputpoints=$((60 - deduct))
 # Size points only apply if you can get 100% on the correctness.
 sizepoints=0
 size=`cat $1 | wc -c`
-if [ $size -le 2554 ]; then sizepoints=5; fi
-if [ $size -le 1762 ]; then sizepoints=15; fi
-if [ $size -le 970 ]; then sizepoints=25; fi
-if [ $size -le 824 ]; then sizepoints=40; fi
+if [ $size -le 2456 ]; then sizepoints=5; fi
+if [ $size -le 1718 ]; then sizepoints=15; fi
+if [ $size -le 980 ]; then sizepoints=25; fi
+if [ $size -le 833 ]; then sizepoints=40; fi
 if [ $differentlines -gt 0 ]; then sizepoints=0; fi
 
 echo "Correctness points: $outputpoints"
--- unit1-exercise-2-grade.sh
+++ unit1-exercise-2-grade.sh
@@ -15,10 +15,10 @@ outputpoints=$((60 - deduct))
 # Size points only apply if you can get 100% on the correctness.
 sizepoints=0
 size=`cat $1 | wc -c`
-if [ $size -le 2332 ]; then sizepoints=5; fi
-if [ $size -le 1764 ]; then sizepoints=15; fi
-if [ $size -le 1196 ]; then sizepoints=25; fi
-if [ $size -le 1016 ]; then sizepoints=40; fi
+if [ $size -le 2408 ]; then sizepoints=5; fi
+if [ $size -le 1814 ]; then sizepoints=15; fi
+if [ $size -le 1220 ]; then sizepoints=25; fi
+if [ $size -le 1037 ]; then sizepoints=40; fi
 if [ $differentlines -gt 0 ]; then sizepoints=0; fi
 
 echo "Correctness points: $outputpoints"
