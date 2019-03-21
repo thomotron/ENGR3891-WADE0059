1847b34bae2e0e74b0a085be2afb05ec42ff5db4
Thomas Wade <wade0059@flinders.edu.au>
2019-03-21 17:12:02 +1030

    o p t i m i s e

--- unit1-exercise-1-solution.sh
+++ unit1-exercise-1-solution.sh
@@ -1,27 +1,28 @@
+M='mkdir -m'
 D=unit1exercise1
-mkdir -m 775 $D
+$M 775 $D
 cd $D
-mkdir -m 210 verkaesung
-mkdir -m 575 zerfahrte
-mkdir -m 005 angeklettheit
-mkdir -m 640 enrauchheit
-mkdir -m 171 ausgepflumen
-mkdir -m 076 auswitzst
-mkdir -m 151 angeschmecken
-mkdir -m 263 aufsprachkeit
-mkdir -m 354 ausgepflumen/aushundkeit
-mkdir -m 566 angeschmecken/auftrittheit
-mkdir -m 012 angeklettheit/gewarfs
-mkdir -m 372 angeschmecken/zerstehheit
-mkdir -m 277 zerfahrte/ausgegehheit
-mkdir -m 667 ausgepflumen/aushundkeit/aufsteher
-mkdir -m 552 angeschmecken/zerstehheit/gesitzer
-mkdir -m 123 ausgepflumen/aushundkeit/anhaltheit
-mkdir -m 065 zerfahrte/ausgegehheit/angekraust
-mkdir -m 407 angeklettheit/gewarfs/ankaesen
-mkdir -m 726 zerfahrte/ausgegehheit/angetritten
-mkdir -m 635 angeschmecken/auftrittheit/ausgeschmeckte
+$M 210 verkaesung
+$M 575 zerfahrte
+$M 005 angeklettheit
+$M 640 enrauchheit
+$M 171 ausgepflumen
+$M 076 auswitzst
+$M 151 angeschmecken
+$M 263 aufsprachkeit
+$M 354 ausgepflumen/aushundkeit
+$M 566 angeschmecken/auftrittheit
+$M 012 angeklettheit/gewarfs
+$M 372 angeschmecken/zerstehheit
+$M 277 zerfahrte/ausgegehheit
+$M 667 ausgepflumen/aushundkeit/aufsteher
+$M 552 angeschmecken/zerstehheit/gesitzer
+$M 123 ausgepflumen/aushundkeit/anhaltheit
+$M 065 zerfahrte/ausgegehheit/angekraust
+$M 407 angeklettheit/gewarfs/ankaesen
+$M 726 zerfahrte/ausgegehheit/angetritten
+$M 635 angeschmecken/auftrittheit/ausgeschmeckte
 S=student
 chown -R $S:$S ./
 cd ..
-tar caf $D.tgz $D
+tar caf unit1-solution1.tgz $D
