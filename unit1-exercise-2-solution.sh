b8e617010e8f35c23885ec00671c6dde77f3fcf0
Thomas Wade <gigatech55@gmail.com>
2019-04-02 08:35:28 +1030

    Add solution for U1E2

--- unit1-exercise-2-solution.sh
+++ unit1-exercise-2-solution.sh
@@ -1,27 +1,26 @@
-D=unit1exercise2
-mkdir $D
-cd $D
-install -o proxy -g dip -m 406 -d zersetzkeit
-install -o nobody -g floppy -m 144 -d begehen
-install -o proxy -g proxy -m 614 -d einhaltt
-install -o mail -g fax -m 014 -d angegehkeit
-install -o lp -g voice -m 217 -d bewitzs
-install -o student -g floppy -m 212 -d aufgewitzheit
-install -o student -g student -m 545 -d ausgehtest
-install -o lp -g floppy -m 406 -d angekaess
-install -o mail -g tape -m 047 -d begehen/berabarbkeit
-install -o mail -g audio -m 703 -d zersetzkeit/enkletts
-install -o lp -g fax -m 054 -d angekaess/zersprachen
-install -o news -g mail -m 731 -d bewitzs/auffahrse
-install -o proxy -g voice -m 163 -d angekaess/angesinnheit
-install -o lp -g cdrom -m 052 -d angekaess/angesinnheit/aufgelaufkeit
-install -o mail -g news -m 057 -d angekaess/angesinnheit/ausgekletttete
-install -o mail -g audio -m 102 -d zersetzkeit/enkletts/gerabarbse
-install -o lp -g voice -m 565 -d angekaess/angesinnheit/einrabarber
-install -o proxy -g tape -m 054 -d bewitzs/auffahrse/ausgewarfkeit
-install -o nobody -g fax -m 603 -d begehen/berabarbkeit/ausgerabarbung
-install -o lp -g dip -m 425 -d angekaess/zersprachen/engehtete
+mkdir unit1exercise2
+cd unit1exercise2
+install -d aufgegeht -m 351 -o uucp -g dip
+install -d angetrittte -m 756 -o proxy -g tape
+install -d angehaltung -m 363 -o mail -g uucp
+install -d angesprachs -m 110 -o lp -g proxy
+install -d gefahrte -m 653 -o news -g voice
+install -d versetztete -m 471 -o mail -g uucp
+install -d ausgesitztest -m 134 -o student -g voice
+install -d angekatzetete -m 051 -o nobody -g fax
+install -d angekatzetete/angehaltst -m 153 -o news -g proxy
+install -d angehaltung/angewitzkeit -m 621 -o student -g voice
+install -d angekatzetete/besinns -m 044 -o nobody -g student
+install -d angehaltung/ausgewarfst -m 630 -o news -g tape
+install -d gefahrte/ausfahrtete -m 057 -o nobody -g student
+install -d angehaltung/angewitzkeit/einfahren -m 675 -o mail -g news
+install -d angekatzetete/angehaltst/verrauchst -m 032 -o mail -g student
+install -d angehaltung/angewitzkeit/aufgesinnt -m 116 -o games -g dip
+install -d angekatzetete/angehaltst/enwitzkeit -m 641 -o games -g mail
+install -d gefahrte/ausfahrtete/ausgetrautest -m 572 -o proxy -g news
+install -d angekatzetete/besinns/angehunden -m 400 -o lp -g tape
+install -d angekatzetete/angehaltst/einrauchs -m 623 -o mail -g news
 cd ..
-chown student:student $D
-chmod 775 $D
-tar caf unit1-solution2.tgz $D
+chown student:student unit1exercise2
+chmod 775 unit1exercise2
+tar caf unit1-solution2.tgz unit1exercise2
