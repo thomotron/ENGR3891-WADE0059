315ad9e38b8e8f02778efc1af23c37ab05932f7f
Thomas Wade <gigatech55@gmail.com>
2019-04-02 07:49:19 +1030

    Added solution for U1E1

--- unit1-exercise-1-solution.sh
+++ unit1-exercise-1-solution.sh
@@ -2,26 +2,26 @@ M='mkdir -m'
 D=unit1exercise1
 $M 775 $D
 cd $D
-$M 210 verkaesung
-$M 575 zerfahrte
-$M 005 angeklettheit
-$M 640 enrauchheit
-$M 171 ausgepflumen
-$M 076 auswitzst
-$M 151 angeschmecken
-$M 263 aufsprachkeit
-$M 354 ausgepflumen/aushundkeit
-$M 566 angeschmecken/auftrittheit
-$M 012 angeklettheit/gewarfs
-$M 372 angeschmecken/zerstehheit
-$M 277 zerfahrte/ausgegehheit
-$M 667 ausgepflumen/aushundkeit/aufsteher
-$M 552 angeschmecken/zerstehheit/gesitzer
-$M 123 ausgepflumen/aushundkeit/anhaltheit
-$M 065 zerfahrte/ausgegehheit/angekraust
-$M 407 angeklettheit/gewarfs/ankaesen
-$M 726 zerfahrte/ausgegehheit/angetritten
-$M 635 angeschmecken/auftrittheit/ausgeschmeckte
+$M 536 angeraucher
+$M 266 aussetzte
+$M 771 angetrautest
+$M 651 enschmeckse
+$M 154 zerfahrer
+$M 664 angekraust
+$M 344 angehunds
+$M 006 aufpflumheit
+$M 616 aussetzte/gehundte
+$M 305 angehunds/angesitzt
+$M 700 aufpflumheit/ausgekaesen
+$M 537 angekraust/geschmecken
+$M 502 angekraust/enkatzetete
+$M 363 angehunds/angesitzt/auspflums
+$M 644 angehunds/angesitzt/aussetzt
+$M 025 angehunds/angesitzt/angeschmecken
+$M 255 angehunds/angesitzt/aufgerennen
+$M 416 angehunds/angesitzt/getrauen
+$M 270 aufpflumheit/ausgekaesen/eingehen
+$M 145 angehunds/angesitzt/aufsetztete
 S=student
 chown -R $S:$S ./
 cd ..
