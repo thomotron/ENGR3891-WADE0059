478090973c8663661ef3b42c3f1b048a4274b966
Paul Gardner-Stephen <paul@servalproject.org>
2019-05-17 15:43:54 +0930

    unit1 files

--- unit1-exercise-3-grade.sh
+++ unit1-exercise-3-grade.sh
@@ -15,10 +15,10 @@ outputpoints=$((60 - deduct))
 # Size points only apply if you can get 100% on the correctness.
 sizepoints=0
 size=`cat $1 | wc -c`
-if [ $size -le 2342 ]; then sizepoints=5; fi
-if [ $size -le 1759 ]; then sizepoints=15; fi
-if [ $size -le 1177 ]; then sizepoints=25; fi
-if [ $size -le 1000 ]; then sizepoints=40; fi
+if [ $size -le 2326 ]; then sizepoints=5; fi
+if [ $size -le 1774 ]; then sizepoints=15; fi
+if [ $size -le 1223 ]; then sizepoints=25; fi
+if [ $size -le 1039 ]; then sizepoints=40; fi
 if [ $differentlines -gt 0 ]; then sizepoints=0; fi
 
 echo "Correctness points: $outputpoints"
